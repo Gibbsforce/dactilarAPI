0ef21d3d7fdcdd0675227472db0634a251f611e2
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-13 20:42:05 -0500

    auth check

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-// Instanciando la clase Connection
-require_once "classes/Connection/Connection.php";
-$Connection = new Connection;
-// ...
-// $query = "INSERT INTO `users-auth` (email,username)value('testing','testingAlb')";
-// print_r($Connection->nonQuery($query));
 ?>
-hello world (index)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dactilar API</title>
+</head>
+<body>
+    <h2>Wlcome to Dactilar API</h2>
+    <h4>An area for developers</h4>
+    <p>We are currently working for the documentation, please visit us later</p>
+</body>
+</html>
