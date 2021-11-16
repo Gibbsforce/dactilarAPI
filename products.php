d663363be421b459c216bcfaf4e74fc1692d6f42
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 11:48:14 -0500

    bugs checking and adding cors controller

--- products.php
+++ products.php
@@ -10,8 +10,9 @@ $Responses = new Responses();
 // $CorsAccessControl = new CorsAccessControl();
 
 // Headers
-header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Origin: https://dactilar.com.pe");
+// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: localhost");
+header("Access-Control-Allow-Origin: https://dactilar.com.pe");
 // header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE");
 // header("Access-Control-Allow-Credentials: true");
 // header("Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With");
