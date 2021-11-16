093a29d2c56154ac373d76e639962abaa4c91222
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 09:34:01 -0500

    bugs checking

--- products.php
+++ products.php
@@ -9,18 +9,19 @@ $Products = new Products();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!isset($_GET["page"]) && !isset($_GET["id"])) {
         $products = $Products->getProducts(1);
-        $header = "Content-Type: application/json";
+        header("Content-Type: application/json");
+        // header("X-Content-Type-Options: nosniff");
         echo json_encode($products);
         http_response_code(200);
     } else if (isset($_GET["page"])) {
         $page = $_GET["page"];
         $products = $Products->getProducts($page);
-        $header = "Content-Type: application/json";
+        header("Content-Type: application/json");
         echo json_encode($products);
         http_response_code(200);
     } else if (isset($_GET["id"])) {
         $id = $_GET["id"];
-        $header = "Content-Type: application/json";
+        header("Content-Type: application/json");
         $product = $Products->getProduct($id);
         echo json_encode($product);
         http_response_code(200);
