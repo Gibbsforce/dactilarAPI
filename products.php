46a7ba60f40f03bdf16e79b5bcb2d96ec5dc3d25
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 10:38:13 -0500

    bugs checking and adding cors controller

--- products.php
+++ products.php
@@ -6,13 +6,12 @@ require_once "classes/Responses.class.php";
 // Instantiating classes
 $Products = new Products();
 $Cors = new CorsAccessControl();
+$Cors->send();
 // $Responses = new Responses();
 // GET
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!isset($_GET["page"]) && !isset($_GET["id"])) {
         $products = $Products->getProducts(1);
-        $Cors->send();
-        print_r($Cors);
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
         // header('Access-Control-Allow-Credentials: true');
@@ -24,7 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     } else if (isset($_GET["page"])) {
         $page = $_GET["page"];
         $products = $Products->getProducts($page);
-        $Cors->send();
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
         // header('Access-Control-Allow-Credentials: true');
@@ -35,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         http_response_code(200);
     } else if (isset($_GET["id"])) {
         $id = $_GET["id"];
-        $Cors->send();
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
         // header('Access-Control-Allow-Credentials: true');
