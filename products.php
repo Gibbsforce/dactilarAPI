c1e4df70516f86266a1fb083688cfaa0b7696871
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 01:58:11 -0500

    checking bugs

--- products.php
+++ products.php
@@ -7,20 +7,26 @@ $Products = new Products();
 // $Responses = new Responses();
 // GET
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if (!isset($_GET["page"]) && !isset($_GET["id"])) $products = $Products->getProducts(1);
-    $header = "Content-Type: application/json";
-    echo json_encode($products);
-    http_response_code(200);
-    if (isset($_GET["page"])) $page = $_GET["page"];
-    $products = $Products->getProducts($page);
-    $header = "Content-Type: application/json";
-    echo json_encode($products);
-    http_response_code(200);
-    if (isset($_GET["id"])) $id = $_GET["id"];
-    $header = "Content-Type: application/json";
-    $product = $Products->getProduct($id);
-    echo json_encode($product);
-    http_response_code(200);
+    if (!isset($_GET["page"]) && !isset($_GET["id"])) {
+        $products = $Products->getProducts(1);
+        $header = "Content-Type: application/json";
+        echo json_encode($products);
+        http_response_code(200);
+    }
+    else if (isset($_GET["page"])) {
+        $page = $_GET["page"];
+        $products = $Products->getProducts($page);
+        $header = "Content-Type: application/json";
+        echo json_encode($products);
+        http_response_code(200);
+    }
+    else if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+        $header = "Content-Type: application/json";
+        $product = $Products->getProduct($id);
+        echo json_encode($product);
+        http_response_code(200);
+    }
 } else {
     header("Content-type: application/json");
     $arr_data = $Responses->error_405();
