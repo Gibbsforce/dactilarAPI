1a25df29d325c90004c9a7453ba8f556d966f46d
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 11:17:18 -0500

    bugs checking and adding cors controller

--- products.php
+++ products.php
@@ -27,7 +27,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     } else if (isset($_GET["page"])) {
         $page = $_GET["page"];
         $products = $Products->getProducts($page);
-        $Cors->send();
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
         // header('Access-Control-Allow-Credentials: true');
@@ -38,7 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         http_response_code(200);
     } else if (isset($_GET["id"])) {
         $id = $_GET["id"];
-        $Cors->send();
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
         // header('Access-Control-Allow-Credentials: true');
