74b3a42c975863fc357dddaa5600ecd6916f030e
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 10:05:20 -0500

    bugs checking

--- products.php
+++ products.php
@@ -9,6 +9,10 @@ $Products = new Products();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!isset($_GET["page"]) && !isset($_GET["id"])) {
         $products = $Products->getProducts(1);
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("Content-Type: application/json");
         header("X-Content-Type-Options: nosniff");
         echo json_encode($products);
@@ -16,21 +20,29 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     } else if (isset($_GET["page"])) {
         $page = $_GET["page"];
         $products = $Products->getProducts($page);
-        header("Content-Type: application/json");
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("X-Content-Type-Options: nosniff");
+        header("Content-Type: application/json");
         echo json_encode($products);
         http_response_code(200);
     } else if (isset($_GET["id"])) {
         $id = $_GET["id"];
-        header("Content-Type: application/json");
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("X-Content-Type-Options: nosniff");
+        header("Content-Type: application/json");
         $product = $Products->getProduct($id);
         echo json_encode($product);
         http_response_code(200);
     }
 } else {
-    header("Content-type: application/json");
     header("X-Content-Type-Options: nosniff");
+    header("Content-type: application/json");
     $arr_data = $Responses->error_405();
     echo json_encode($arr_data);
 }
