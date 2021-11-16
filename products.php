e854842831cddf41c8a44e8495895976711298c9
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-16 11:45:32 -0500

    bugs checking and adding cors controller

--- products.php
+++ products.php
@@ -1,27 +1,24 @@
 <?php
 // Accessing to the products and responses classes
 require_once "classes/Products.class.php";
-require_once "classes/CorsAccessControl.class.php";
 require_once "classes/Responses.class.php";
+// require_once "classes/CorsAccessControl.class.php";
+
 // Instantiating classes
 $Products = new Products();
+$Responses = new Responses();
+// $CorsAccessControl = new CorsAccessControl();
 
-$Cors = new CorsAccessControl();
-$domain = "https://dactilar.com.pe";
-$Cors->add($domain);
-$head = $Cors->send();
-print_r($head);
+// Headers
+header("Access-Control-Allow-Origin: https://dactilar.com.pe");
+// header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE");
+// header("Access-Control-Allow-Credentials: true");
+// header("Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With");
 
-// header('Access-Control-Allow-Origin: *');
-// $Responses = new Responses();
 // GET
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!isset($_GET["page"]) && !isset($_GET["id"])) {
         $products = $Products->getProducts(1);
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
-        // header('Access-Control-Allow-Credentials: true');
-        // header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("X-Content-Type-Options: nosniff");
         header("Content-Type: application/json");
         echo json_encode($products);
@@ -29,20 +26,12 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     } else if (isset($_GET["page"])) {
         $page = $_GET["page"];
         $products = $Products->getProducts($page);
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
-        // header('Access-Control-Allow-Credentials: true');
-        // header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("X-Content-Type-Options: nosniff");
         header("Content-Type: application/json");
         echo json_encode($products);
         http_response_code(200);
     } else if (isset($_GET["id"])) {
         $id = $_GET["id"];
-        // header('Access-Control-Allow-Origin: *');
-        // header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
-        // header('Access-Control-Allow-Credentials: true');
-        // header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
         header("X-Content-Type-Options: nosniff");
         header("Content-Type: application/json");
         $product = $Products->getProduct($id);
