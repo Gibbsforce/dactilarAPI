7df26d675657da36e51dec97ed493317d32758db
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-17 20:18:52 -0500

    products api for post and ignoring public folder

--- classes/Connection/Connection.php
+++ classes/Connection/Connection.php
@@ -1,5 +1,5 @@
 <?php
-// Creando la clase Connection para conectarse a la base de datos
+// Building the Connection class in order to connect to a database
 class Connection {
     private $server;
     private $user;
@@ -7,11 +7,11 @@ class Connection {
     private $database;
     private $port;
     private $connection;
-    // Iniciando el constructor
+    // Initializing the constructor
     function __construct() {
-        // Almacenando los datos para la configuracion en la base de datos de la funcion en una variable
+        // Storing the data config extensionless
         $data_list = $this->dataConnection();
-        // Recorriendo los datos
+        // Looping trough the data config
         foreach($data_list as $key => $value) {
             $this->server = $value["server"];
             $this->user = $value["user"];
@@ -19,7 +19,7 @@ class Connection {
             $this->database = $value["database"];
             $this->port = $value["port"];
         }
-        // Estableciendo la conexion
+        // Initializing the connection
         $this->connection = new mysqli(
             $this->server,
             $this->user,
@@ -27,26 +27,26 @@ class Connection {
             $this->database,
             $this->port
         );
-        // Manejando los errores de conexion
+        // Handling the connection error
         if ($this->connection->connect_errno) {
-            echo "No se ha podido establecer una conexion";
+            echo "Couldn't connect";
             die();
         }
     }
-    // Funcion que obtiene un json encode de los datos para la configuracion en la base de datos
+    // Method that gets a json encode for config connection data
     private function dataConnection() {
         $dir = dirname(__FILE__);
         $json_data = file_get_contents($dir."/"."config");
         return json_decode($json_data, true);
     }
-    // Funcion que convierte los caracteres a la codificacion de UTF8
+    // Method that converts the charset into UTF8
     private function toUTF8($arr) {
         array_walk_recursive($arr, function(&$item, $key) {
             if (!mb_detect_encoding($item, "utf-8", true)) return $item = utf8_encode($item);
         });
         return $arr;
     }
-    // Obtener los datos de una tabla de la base de datos
+    //  Getting the data of a table of the database
     public function getData($query) {
         $result =  $this->connection->query($query);
         $arr_result = array();
@@ -55,13 +55,13 @@ class Connection {
         }
         return $this->toUTF8($arr_result);
     }
-    // Agregar datos a una tabla y devolviendo la cantidad de filas afectadas
+    // Inserting data into a table and returning the affected rows
     public function nonQuery($query) {
         $this->connection->set_charset("utf8");
         $result =  $this->connection->query($query);
         return $this->connection->affected_rows;
     }
-    // Agrega datos y devuelve el id la fila que se agrego
+    // Inserting the data en return the id of added row
     public function nonQueryId($query) {
         $this->connection->set_charset("utf8");
         $result =  $this->connection->query($query);
@@ -69,7 +69,7 @@ class Connection {
         if ($rows > 0) return $this->connection->insert_id;
         return 0;
     }
-    // Metodo que encripta por md5 los datos (contrasena)
+    // Method that encrypts data by md5 (password ie)
     protected function encrypt ($str) {
         return md5($str);
     }
