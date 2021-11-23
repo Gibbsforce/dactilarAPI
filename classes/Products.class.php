c265972f1a36ff5ae535e7542d379f257d2eaca5
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-23 11:08:43 -0500

    adding date update on update product method put rest

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -268,6 +268,7 @@ class Products extends Connection {
     }
     // Updating products method
     private function updateProduct($product_uid) {
+        $this->product_date = date("Y-m-d H:i");
         $query = "UPDATE ".$this->table." SET
             `product_name` = '".$this->product_name."',
             `product_class` = '".$this->product_class."',
@@ -281,7 +282,8 @@ class Products extends Connection {
             `product_sizes` = '".$this->product_sizes."',
             `product_image` = '".$this->product_image."',
             `product_images_gallery` = '".$this->product_images_gallery."',
-            `product_images_thumbnails` = '".$this->product_images_thumbnails."'
+            `product_images_thumbnails` = '".$this->product_images_thumbnails."',
+            `product_date` = '".$this->product_date."'
             WHERE `product_id` = '".$this->product_id."' OR `product_uid` = '$product_uid'";
         try {
             $product = parent::nonQuery($query);
