07076bdadf60399346e472d20a07f2ed72d12475
Alvaro Avalos <tlalvaro15@gmail.com>
2021-11-30 23:08:59 -0500

    consuming validating email when signing up

--- auth.php
+++ auth.php
@@ -6,7 +6,7 @@ require_once "classes/Responses.class.php";
 $Auth = new Auth;
 $Responses = new Responses;
 print_r("testing... dotenv");
-print_r(getenv("SERVER"));
+print_r(getenv());
 // Validando metodo post
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET["uid"]) && isset($_GET["token"])) {
