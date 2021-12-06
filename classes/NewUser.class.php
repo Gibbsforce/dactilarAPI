43d08930fdb978abbcdd953b1e8bb8c09997d4ab
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-06 18:25:24 -0500

    styling email veryfication and some cors

--- classes/NewUser.class.php
+++ classes/NewUser.class.php
@@ -200,8 +200,12 @@ class NewUser extends Connection {
         $url = "https://".$_SERVER["SERVER_NAME"]."/auth?id=".$id_users."&uid=".$unique_id."&token=".$token."";
         $subject = "Account Validation - Dactilar";
         $body = "
-            <html>
+                <!DOCTYPE html>
+                <html lang='en'>
                 <head>
+                    <meta charset='UTF-8'>
+                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
                     <title>Email validation</title>
                     <style>
                         * {
