ae7de0854a7310377ae496f2834c752b889ff852
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-06 17:36:29 -0500

    styling email veryfication and some cors

--- classes/NewUser.class.php
+++ classes/NewUser.class.php
@@ -312,12 +312,7 @@ class NewUser extends Connection {
                     If you did not request this email, please ignore it.
                 </p>
             </div>
-            <script>
-                const btn = document.querySelector('.btn-url');
-                btn.addEventListener('click', () => {
-                    window.location.href = '".$url."';
-                });
-            </script>
+            
         </body>
         </html>
         ";
