6edd938af2a3df3e009945ae5fd2c9b432b7a36b
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-06 17:50:54 -0500

    styling email veryfication and some cors

--- newuser.php
+++ newuser.php
@@ -8,6 +8,7 @@ $Responses = new Responses;
 // Headers
 // CORS
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With");
