b8d8964d4b2f9ee4d841f5e451ce9709ceb57c28
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-11 00:20:04 -0500

    adding getting all data products

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -32,6 +32,8 @@ class Products extends Connection {
         $start = 0;
         $qty = 15;
         if ($page > 1) $start = $qty * ($page - 1);
+        $query_total = "SELECT `product_id` FROM ".$this->table."";
+        $total = parent::getData($query_total); // maybe check this in the future
         $query =
             "SELECT 
                 `product_id`,
@@ -49,10 +51,10 @@ class Products extends Connection {
             if (!isset($data)) return $this->Responses->error_500();
             // test for consuming api in react
             $res = array(
-                "page" => $page,
+                "page" => intval($page),
                 "results" => $data,
-                "total_pages" => ceil(count($data) / $qty),
-                "total_results" => count($data),
+                "total_pages" => ceil(count($total) / $qty),
+                "total_results" => count($total),
             );
             return $res;
         } catch (PDOException $error) {
