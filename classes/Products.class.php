1c3b7fd7028319e9b5cb801abf84bcae679c3296
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-10 23:46:27 -0500

    adding getting all data products

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -92,8 +92,8 @@ class Products extends Connection {
         $start = 0;
         $qty = 10;
         if ($page > 1) $start = $qty * ($page - 1);
-        $query_total = "SELECT `product_id` FROM ".$this->table."";
-        print_r($query_total);
+        $query_total = "SELECT COUNT(*) FROM ".$this->table."";
+        $query_total = parent::getData($query_total);
         $query =
             "SELECT 
                 `product_id`,
