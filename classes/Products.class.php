54f612e66aedf686e5adc9a802c9fe2293df9d93
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-10 23:42:29 -0500

    adding getting all data products

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -92,6 +92,7 @@ class Products extends Connection {
         $start = 0;
         $qty = 10;
         if ($page > 1) $start = $qty * ($page - 1);
+        $query_total = "SELECT COUNT(*) FROM ".$this->table."";
         $query =
             "SELECT 
                 `product_id`,
@@ -117,7 +118,7 @@ class Products extends Connection {
             $products = array(
                 "page" => $page,
                 "results" => $data,
-                "total_pages" => ceil(count($data) / $qty),
+                "total_pages" => ceil(count($query_total) / $qty),
                 "total_results" => count($data),
             );
             return $products;
