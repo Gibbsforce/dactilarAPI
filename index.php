d6d26a5a815e8f1bb2ad8eeba5b788c7011dc838
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-18 14:35:22 -0500

    setting cors to the index

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+// Headers
+// CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 ?>
 <!DOCTYPE html>
 <html lang="en">
