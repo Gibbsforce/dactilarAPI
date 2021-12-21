616915829e3fb728a15f977fb35132a44ada96ba
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-21 09:57:38 -0500

    endpoint for get the base64 image by uid added

--- products.php
+++ products.php
@@ -27,50 +27,57 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 
 // GET
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    if (isset($_GET["all"])) {
-        $all_products = $Products->getAllProducts(1);
-        header("Content-Type: application/json");
-        echo json_encode($all_products);
-        http_response_code(200);
-    } else if (isset($_GET["pages"])) {
-        $pages = $_GET["pages"];
-        $all_products = $Products->getAllProducts($pages);
-        header("Content-Type: application/json");
-        echo json_encode($all_products);
-        http_response_code(200);
-    } else if (isset($_GET["uid"])) {
-        $uid = $_GET["uid"];
-        $all_product = $Products->getAllProduct($uid);
-        header("Content-Type: application/json");
-        echo json_encode($all_product);
-        http_response_code(200);
-    } else if (!isset($_GET["page"]) && !isset($_GET["id"])) {
-        $products = $Products->getProducts(1);
-        header("Content-Type: application/json");
-        echo json_encode($products);
-        http_response_code(200);
-    } else if (isset($_GET["page"])) {
-        $page = $_GET["page"];
-        $products = $Products->getProducts($page);
-        header("Content-Type: application/json");
-        echo json_encode($products);
-        http_response_code(200);
-    } else if (isset($_GET["id"])) {
-        $id = $_GET["id"];
-        header("Content-Type: application/json");
-        $product = $Products->getProduct($id);
-        echo json_encode($product);
-        http_response_code(200);
-    } else if (isset($_GET["thumbnails_uid"])) {
+    // if (isset($_GET["all"])) {
+    //     $all_products = $Products->getAllProducts(1);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($all_products);
+    //     http_response_code(200);
+    // } else if (isset($_GET["pages"])) {
+    //     $pages = $_GET["pages"];
+    //     $all_products = $Products->getAllProducts($pages);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($all_products);
+    //     http_response_code(200);
+    // } else if (isset($_GET["uid"])) {
+    //     $uid = $_GET["uid"];
+    //     $all_product = $Products->getAllProduct($uid);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($all_product);
+    //     http_response_code(200);
+    // } else if (!isset($_GET["page"]) && !isset($_GET["id"])) {
+    //     $products = $Products->getProducts(1);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($products);
+    //     http_response_code(200);
+    // } else if (isset($_GET["page"])) {
+    //     $page = $_GET["page"];
+    //     $products = $Products->getProducts($page);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($products);
+    //     http_response_code(200);
+    // } else if (isset($_GET["id"])) {
+    //     $id = $_GET["id"];
+    //     header("Content-Type: application/json");
+    //     $product = $Products->getProduct($id);
+    //     echo json_encode($product);
+    //     http_response_code(200);
+    // } else if (isset($_GET["thumbnails_uid"])) {
+    //     $thumbnails_uid = $_GET["thumbnails_uid"];
+    //     $thumbnails = $Products->getThumbnails($thumbnails_uid);
+    //     header("Content-Type: application/json");
+    //     echo json_encode($thumbnails);
+    //     http_response_code(200);
+    // } else {
+    //     header("Content-type: application/json");
+    //     $arr_data = $Responses->error_405();
+    //     echo json_encode($arr_data);
+    // }
+    if (isset($_GET["thumbnails_uid"])) {
         $thumbnails_uid = $_GET["thumbnails_uid"];
         $thumbnails = $Products->getThumbnails($thumbnails_uid);
         header("Content-Type: application/json");
         echo json_encode($thumbnails);
         http_response_code(200);
-    } else {
-        header("Content-type: application/json");
-        $arr_data = $Responses->error_405();
-        echo json_encode($arr_data);
     }
 // POST
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
