9c66d36f7edfca57c8ad121961b53c7024ed07c2
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-21 12:17:05 -0500

    endpoint for get the base64 image by uid added

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -173,6 +173,7 @@ class Products extends Connection {
             // convert data url thumbs to base 64
             $data[0]["product_image"] = $this->toBase64Image($data[0]["product_image"]);
             $arr_thumbs = explode(",", $data[0]["product_images_gallery"]);
+            print_r($arr_thumbs);
             $data[0]["product_images_gallery"] = $this->toBase64Images($arr_thumbs);
             return $data;
         } catch (PDOException $error) {
