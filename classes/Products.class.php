58d45f554fc3913eef94214c1bf43c9f7bdbf276
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-21 10:38:24 -0500

    endpoint for get the base64 image by uid added

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -402,7 +402,7 @@ class Products extends Connection {
     // Methods that proccess the data products
     // Base 64 encoding
     private function toBase64Image($image_url) {
-        if (!$image_url) return "";
+        if (!$image_url) return false;
         $image_data = file_get_contents($image_url);
         $finfo = new finfo(FILEINFO_MIME_TYPE);
         $mime_content_type = $finfo->buffer($image_data);
@@ -412,7 +412,7 @@ class Products extends Connection {
   
     }
     private function toBase64Images($images_url) {
-        if (count($images_url) === 0) return [];
+        if (count($images_url) === 0) return false;
         $images_base64 = array();
         foreach ($images_url as $image_url) {
             $image_data = file_get_contents($image_url);
