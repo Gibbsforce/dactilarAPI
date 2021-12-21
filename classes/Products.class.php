06609e505986098d50fa8e6083ee12c6c047442b
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-21 09:52:00 -0500

    endpoint for get the base64 image by uid added

--- classes/Products.class.php
+++ classes/Products.class.php
@@ -172,8 +172,8 @@ class Products extends Connection {
             $data = parent::getData($query);
             if (!isset($data)) return $this->Responses->error_500();
             // convert data url thumbs to base 64
-            $data[0]["product_image"] = $this->toBase64Image($data[0]["product_image"]);
-            $data[0]["product_images_gallery"] = $this->toBase64Images($data[0]["product_images_gallery"]);
+            // $data[0]["product_image"] = $this->toBase64Image($data[0]["product_image"]);
+            // $data[0]["product_images_gallery"] = $this->toBase64Images($data[0]["product_images_gallery"]);
             return $data;
         } catch (PDOException $error) {
             return Responses::prepare(500, $error->getMessage());
