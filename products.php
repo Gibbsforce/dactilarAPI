b34ece581f064e2180750d91ca6d4f52c2131a50
Alvaro Avalos <tlalvaro15@gmail.com>
2021-12-22 12:22:23 -0500

    put cors to products

--- products.php
+++ products.php
@@ -14,7 +14,7 @@ $Responses = new Responses();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 // header("Access-Control-Allow-Origin: https://dactilar.com.pe");
-// header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE");
+header("Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE");
 // header("Access-Control-Allow-Credentials: true");
 // header("Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With");
 
