32f18948e3368879ef41d22c1fdddb901291842a
Alvaro Avalos <tlalvaro15@gmail.com>
2022-01-21 15:36:12 -0500

    put for update user

--- classes/Users.class.php
+++ classes/Users.class.php
@@ -222,6 +222,8 @@ class Users extends Connection {
             $this->username = $data["username"];
         }
         $result_user_exist = $this->existingUser($this->dni, $this->email, $this->username);
+        print_r($result_user_exist);
+        print_r($this->username);
         if ($result_user_exist[0]["dni"] === $this->dni) return $Responses->error_200("DNI number already exists");
         if ($result_user_exist[0]["email"] === $this->email) return $Responses->error_200("The email already exists");
         if (strtolower($result_user_exist[0]["username"]) === strtolower($this->username)) return $Responses->error_200("The username already exists");
@@ -230,7 +232,7 @@ class Users extends Connection {
             if (!$added) return $Responses->error_500();
             $response = $Responses->response;
             $response["result"] = array(
-                "username" => $this->username
+                "username" => $this->uname
             );
             return $response;
         } catch (PDOException $error) {
